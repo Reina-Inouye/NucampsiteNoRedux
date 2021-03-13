44013ab25c01c45b54d7164fc0c1b62e86ff54fd
Reina Inouye <reina.inouye@gmail.com>
2021-03-13 10:05:31 -1000

    Week 3 assigment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,34 +1,33 @@
 import React from "react";
-import {Card, CardImg, CardText, CardBody, CardTitle}from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
+function RenderCampsite({ campsite }) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
 
-function RenderCampsite({campsite}) {
-    return(
-        <div className ="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-
-        </div>    
+        </div>
     );
 };
 
 
-function RenderComments({comments}) {
+function RenderComments({ comments }) {
     if (comments) {
-        return(
+        return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
                 {comments.map(comment => {
-                    return(
+                    return (
                         <div key={comment.id}>
-                        <p>{comment.text}<br/>
-                        --{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            <p>{comment.text}<br />
+                        --{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                         </div>
                     );
                 })
@@ -36,7 +35,7 @@ function RenderComments({comments}) {
             </div>
         );
     }
-    return <div/> // Returns an empty div if there are no comments
+    return <div /> // Returns an empty div if there are no comments
 }
 
 
@@ -44,16 +43,26 @@ function RenderComments({comments}) {
 function CampsiteInfo(props) {
     if (props.campsite) {
         return (
-        <div className = "container">  
-            <div className = "row">
-                <RenderCampsite campsite={props.campsite}/>
-                <RenderComments comments = {props.comments} />
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
+                </div>
             </div>
-        </div>
         );
     }
     return (
-        <div/>
+        <div />
     );
 }
 
